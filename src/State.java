e18403d386a7bcb8bd19a3a19e925a963b144749
Danté G <ineffableSkillz@users.noreply.github.com>
2017-10-08 09:54:19 +0100

    Changed goesTo() return type & Implemented setOwner(StateSet stateSet)

--- src/State.java
+++ src/State.java
@@ -2,9 +2,9 @@ import java.util.HashMap;
 
 public class State {
 
-	private int nodeNumber;
+	private int nodeNumber; //Dead State Special Value: 666 [Needs a method for it to point to itself]
 	private HashMap<Outputs, State> transition = new HashMap<>();
-
+	private StateSet belongsTo;
 	
 	State(int nodeNumber) {
 		setNodeNumber(nodeNumber);
@@ -22,13 +22,27 @@ public class State {
 		transition.put(key, value);
 	}
 	
+	HashMap<Outputs, State> getTransitions() {
+		return transition;
+	}
+	
 	/**
 	 * 
 	 * @param key
 	 * @return The number of the node the chosen transition points to
 	 */
-	int goesTo(Outputs key) {
-		return(transition.get(key).getNodeNumber());
+	State goesTo(Outputs key) {
+		return(transition.get(key));
 	}
 	
+	StateSet getBelongingSet() {
+		return belongsTo;
+	}
+
+	void setOwner(StateSet stateSet) {
+		belongsTo = stateSet;
+	}
 }
+
+
+/* Changed goesTo return type & setOwnerMethod*/
