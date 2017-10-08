9c36b6aa824fd630d1262b23c887e61adf3a215e
Danté G <ineffableSkillz@users.noreply.github.com>
2017-10-08 09:57:09 +0100

    Git command confusion

--- src/StateSet.java
+++ src/StateSet.java
@@ -1,13 +1,63 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 public class StateSet {
 	
 	private boolean toSplit = false;
+	private List<State> listOfStates = new ArrayList<>();	
 	
 	void markToSplit() {
 		toSplit = true;
 	}
 	
+	void resetMark() {
+		toSplit = false;
+	}
+	
 	boolean isToSplit() {
 		return toSplit;
+	}
+	
+	void addState(State state) {
+		listOfStates.add(state);
+		state.setOwner(this);
+		
+	}
+	
+	List<State> getListOfStates() {
+		return listOfStates;
+	}
+	
+	/**
+	 * Checks for any collisions (row by row) and updates the toSplit variable's value
+	 * accordingly.
+	 */
+	void checkCollisions(int max) {
+		
+		int counter = 0;
+		
+		for(Outputs transitionValue : Outputs.values()) { //For each row (input)
+			
+			/* Base Case - Maximum Number of Transitions Values on a node */
+			if(counter >= max)
+				return;
+			
+			HashSet<StateSet> setOfResults = new HashSet<>();
+			
+			/* Check which set current input takes it to (Due to Dead State, will always return not null*/
+			for(State node : listOfStates)  
+				setOfResults.add(node.goesTo(transitionValue).getBelongingSet()); 
+			
+			/* If HashSet is more than 1, outputs for row are going to more than 1 set */
+			if(setOfResults.size() > 1) {
+				markToSplit();
+				break;
+			}
+			
+			counter++;
+			
+		}
+
 	}
 }
---
+++ tests/TestStateSet.java
@@ -0,0 +1,84 @@
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestStateSet {
+
+	State state0, state1, state2, state3, dead;
+	List<State> stateCollection = new ArrayList<>();
+	
+	@Before
+	public void setUp() {
+		
+		state0 = new State(0);
+		state1 = new State(1);
+		state2 = new State(2);
+		state3 = new State(3);
+		dead = new State(666);
+		
+		stateCollection.add(state0);
+		stateCollection.add(state1);
+		stateCollection.add(state2);
+		stateCollection.add(state3);
+		stateCollection.add(dead);
+	}
+
+	/* Could Transfer to Project */
+	public void mapToDead(int numberOutputs) {
+		
+		for(State s : stateCollection) {
+			int counter = 0;
+			HashMap<Outputs, State> temp = s.getTransitions();
+			
+			for(Outputs transValue : Outputs.values()) {
+				
+				/* Base Case - Number of output values */
+				if(counter >= numberOutputs)
+					break;
+				
+				/* If there isn't a key, there isn't a transition and will thus set to point to dead state */
+				if(!temp.containsKey(transValue))
+					s.setTransition(transValue, dead);
+				
+				counter++;
+			}
+			
+			
+		}
+	}
+	
+	@Test
+	public void collisionTest1() {
+		int maxTransitions = 2;
+		
+		StateSet accepting = new StateSet();
+		StateSet others = new StateSet();
+			
+		state0.setTransition(Outputs.A, state1);
+		state1.setTransition(Outputs.A, state2);
+		state2.setTransition(Outputs.B, state3);
+		mapToDead(maxTransitions);
+		
+		/* Add Accepting States */
+		accepting.addState(state1);
+		accepting.addState(state3);
+		
+		/* Add Non-Accepting States */
+		others.addState(state0);
+		others.addState(state2);
+		others.addState(dead);
+		
+		accepting.checkCollisions(maxTransitions);
+		others.checkCollisions(maxTransitions);
+
+		assertTrue(accepting.isToSplit() == true);
+		assertTrue(others.isToSplit() == true);
+		
+	}
+
+}
