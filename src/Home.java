8118fa3d1436dc2ea2cd4542f4e199cd1598a932
Danté G <ineffableSkillz@users.noreply.github.com>
2017-10-15 10:31:42 +0100

    Update
    1. Added Input checking method
    2. Added the Dead State within handleInput() with special value 666
    3. Added getTransition() - Still In process
    4. Added getNode() - In process. Will be used for the final part of getTransition

--- src/Home.java
+++ src/Home.java
@@ -2,38 +2,144 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+/* Can Alter for more than 3 transition values later */
 
 public class Home {
 	
 	/* Manages all of the sets */
-	static List<StateSet> listOfStates = new ArrayList<>();
+	static List<StateSet> listOfSets = new ArrayList<>();
 	
 	/* Initial 2 Sets: S0 and S1. Will be redundant after first split */
 	static StateSet accepting = new StateSet();
 	static StateSet other = new StateSet();
 
+	/* Holds Number of Nodes - For minor error handling */
+	static int maxNodes;
+	
+	
+	
 	
 	/* Use args for state input, separate via common */
 	public static void main(String[] args) {
-		
-	handleInput(args);
-		
+
 	/* Adding S0 and S1 to listOfStates */
-	listOfStates.add(accepting);
-	listOfStates.add(other);
+	listOfSets.add(accepting);
+	listOfSets.add(other);
+	
+	handleInput(args);
+	getTransitions();
 		
-		
+	
 	}
 	
 	
+	private static void getTransitions() {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		/* Adding All States to a single list for efficiency */
+		List<State> listOfStates= new ArrayList<>();
+		maxNodes = listOfStates.size();
+		
+		List<State> temp1 = accepting.getListOfStates();
+		List<State> temp2 = other.getListOfStates();
+		
+		for(State state : temp1)
+			 listOfStates.add(state);
+		
+		for(State state : temp2)
+			 listOfStates.add(state);
+		
+		
+		/* Transition input for each state*/
+		for(State state : listOfStates) {
+			
+			String tempHolder;
+			int[] transitionNodeNumber = new int[3];
+			
+			/* Getting User Input */
+			System.out.println("a b c Transition for Node #" + state.getNodeNumber());
+			tempHolder = scanner.nextLine();
+			
+			/* Checking User Input */
+			while(!isValidInput(tempHolder, transitionNodeNumber)) {
+				System.out.println("a b c Transition for Node #" + state.getNodeNumber());
+				System.out.println("For no transition, enter \"666\". ");
+				tempHolder = scanner.nextLine();
+			}
+			
+			/* Adding Information to States */
+			state.setTransition(Outputs.A, transitionNodeNumber[0]);
+			state.setTransition(Outputs.B, transitionNodeNumber[1]);
+			state.setTransition(Outputs.C, transitionNodeNumber[2]);
+			
+			
+			scanner.close();
+		}
+		
+	}
+
+	private static State getNode(int value, List<State> list) {
+		
+		for(State state : list) {
+			
+			if(state.getNodeNumber() == value)
+				return state;
+			
+		}
+		
+		return null;
+		
+	}
+
+	private static boolean isValidInput(String input, int[] intAnswers) {
+		
+		String[] answers = input.split(" ");
+		
+		if(answers.length != 3)
+			return false;
+		
+		
+		
+		try {
+			
+			for(int x = 0; x < 3; x++)
+				intAnswers[x] = Integer.parseInt(answers[x]);
+			
+		} catch (NumberFormatException e) {
+			
+			System.out.println("Non-numerical Value Enteres for transitions");
+			System.out.println(e);
+			
+		}
+		
+		
+		for(int x : intAnswers)
+			if(x > maxNodes -1 || x < 0) {
+				
+				System.out.println("Invalid Node Number Entered");
+				return false;
+			}
+		
+		return true;
+		
+		
+		
+	}
+
+
 	/**
 	 * Handles initial state input. All numbers prior to ',' in args
 	 * belong to the accepting state.
-	 * @param args Contains numbers and a comma which is used to seperate the 
+	 * @param args Contains numbers and a comma which is used to separate the 
 	 * initial accepting and non-accepting states
 	 */
 	private static void handleInput(String[] args) {
 		
+		
+		/* Handling Args Input */
 		boolean addingAccepting = true;
 		
 		for(String x : args) {
@@ -51,8 +157,9 @@ public class Home {
 				
 			}
 			
-			
-		}
+		}	
+		
+		other.addState(new State(666));
 		
 	}
 
